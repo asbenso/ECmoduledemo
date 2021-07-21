b55a10d73ec6a55c53213657db0baec72a8cc989
unknown <besong79@yahoo.com>
2021-07-22 00:15:35 +0100

    Levrage output to print private and public IP address of ec2 instance

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ module "jjtech-ec2" {
   name          = "jjtech-server"
 
 }
+
+output "PrivateIP" {
+  value = module.jjtech-ec2.private-ip
+}
+
+output "PublicIP" {
+  value = module.jjtech-ec2.public-ip
+}
