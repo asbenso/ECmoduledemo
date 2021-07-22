f881d9cdc29363e1b7e04a6cddfd6c671e388693
unknown <besong79@yahoo.com>
2021-07-22 01:52:12 +0100

    Used parameters in main tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 module "jjtech-ec2" {
   source        = "github.com/asbenso/Modules//EC2"
-  ami_id        = "ami-0dc2d3e4c0f9ebd18"
-  instance_type = "t2.micro"
-  name          = "jjtech-server"
-
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  name          = var.name
 }
